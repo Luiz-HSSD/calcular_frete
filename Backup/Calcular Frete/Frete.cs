596419695803b87244acb7542add82bc6efc6ac0
Luiz Henrique Santos Sousa Diniz <lhenrique_diniz@hotmail.com>
2017-03-29 15:06:49 -0300

    implementado versão 1.0

---
+++ Backup/Calcular Frete/Frete.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WindowsFormsApplication1.br.com.correios.ws;
+
+namespace WindowsFormsApplication1
+{
+    public class Frete
+    {
+        public Frete() 
+        {
+            ws = new CalcPrecoPrazoWS();
+        }
+        private CalcPrecoPrazoWS ws;
+        private decimal comprimento;
+        public decimal Comprimento
+        {
+            get { return comprimento; }
+            set { comprimento = value; }
+        }
+        private decimal altura;
+        public decimal Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+        private decimal largura;
+        public decimal Largura
+        {
+            get { return largura; }
+            set { largura = value; }
+        }
+        private string peso;
+        public string Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+        private string cep;
+        public string CEP
+        {
+            get { return cep; }
+            set { cep = value; }
+        }
+        public cResultado calcular() 
+        {
+            return ws.CalcPrecoPrazo("", "", "40010 , 40045 , 40215 , 40290 , 41106", "08563010", CEP, Peso, 1, Comprimento, Altura, Largura, 0, "N", 17, "S");
+        } 
+    }
+}
---
+++ Calcular Frete/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WindowsFormsApplication1.br.com.correios.ws;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Frete freteagr = new Frete();
+        cResultado resultado;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            freteagr.Comprimento =Convert.ToDecimal(textBox1.Text);
+            freteagr.Largura = Convert.ToDecimal(textBox2.Text);
+            freteagr.Altura = Convert.ToDecimal(textBox3.Text);
+            freteagr.Peso = textBox4.Text;
+            freteagr.CEP = textBox5.Text;
+            resultado = freteagr.calcular();
+            MessageBox.Show("Sedex Varejo \n Preço:" + resultado.Servicos.ElementAt(0).Valor + "\n Prazo em dias:" + resultado.Servicos.ElementAt(0).PrazoEntrega + "\n" + resultado.Servicos.ElementAt(0).MsgErro + "\n Sedex A Cobrar Varejo \n Preço:" + resultado.Servicos.ElementAt(1).Valor + "\n Prazo em dias:" + resultado.Servicos.ElementAt(1).PrazoEntrega + "\n" + resultado.Servicos.ElementAt(1).MsgErro + "\n Sedex 10 Varejo \n Preço:" + resultado.Servicos.ElementAt(2).Valor + "\n Prazo em dias:" + resultado.Servicos.ElementAt(2).PrazoEntrega + "\n" + resultado.Servicos.ElementAt(2).MsgErro + "\nSedex Hoje Varejo \n Preço:" + resultado.Servicos.ElementAt(3).Valor + "\n Prazo em dias:" + resultado.Servicos.ElementAt(3).PrazoEntrega + "\n" + resultado.Servicos.ElementAt(3).MsgErro + "\n PAC Varejo \n Preço:" + resultado.Servicos.ElementAt(4).Valor + "\n Prazo em dias:" + resultado.Servicos.ElementAt(4).PrazoEntrega + "\n" + resultado.Servicos.ElementAt(4).MsgErro);
+        }
+    }
+}
